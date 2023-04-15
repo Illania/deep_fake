a309ac786de664198f5a1e1eb9f780b3699807f8
Anna Gulich <ag@nordic-it.com>
2023-04-15 13:12:00 +0300

    deleted old implementation files

--- AutoEncoders/autoencoder.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AutoEncoder(metaclass=ABCMeta):
-    """ Base face detector class. """
-
-    @abstractmethod
-    def compile(self, optimizer, loss):
-        pass
--- FaceDetectors/face_detector.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class FaceDetector(metaclass=ABCMeta):
-    """ Base face detector class. """
-
-    @abstractmethod
-    def detect_face(self, image_path):
-        pass
