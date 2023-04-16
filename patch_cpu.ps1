d844426c7be45e6c2b260996116948d69a397bd4
Sergey Shvets M <recycletechno@gmail.com>
2023-04-16 15:27:49 +0300

    First commit after success install of SimSwap on M2

--- patch_cpu.ps1
+++ patch_cpu.ps1
@@ -1,7 +1,3 @@
-# ПАЧЬ: https://pastebin.com/zu1yQ5C0
-# 1. Закинуть в папку с симсвопом
-# 2. Запустить
-
 # Делает симсвоп ЦП-совместимым, а не просто эксклюзивным. Отдельную версию торча вроде как качать не нужно.
 
 <# :
@@ -18,10 +14,15 @@ $search_replace = @{
     "if len(self.opt.gpu_ids)" = "if torch.cuda.is_available() and len(self.opt.gpu_ids)";
     "torch.device(`"cuda:0`")" = "torch.device(`'cuda:0`' if torch.cuda.is_available() else `'cpu`')";
     "torch.load(netArc_checkpoint)" = "torch.load(netArc_checkpoint) if torch.cuda.is_available() else torch.load(netArc_checkpoint, map_location=torch.device(`'cpu`'))";
-	"net.load_state_dict(torch.load(save_pth))" = "net.load_state_dict(torch.load(save_pth)) if torch.cuda.is_available() else net.load_state_dict(torch.load(save_pth, map_location=torch.device(`'cpu`')))";
+    "net.load_state_dict(torch.load(save_pth))" = "net.load_state_dict(torch.load(save_pth)) if torch.cuda.is_available() else net.load_state_dict(torch.load(save_pth, map_location=torch.device(`'cpu`')))";
     ".cuda()" = ".to(torch.device(`'cuda:0`' if torch.cuda.is_available() else `'cpu`'))";
     ".to('cuda')" = ".to(torch.device(`'cuda:0`' if torch.cuda.is_available() else `'cpu`'))";
 }
+
+if (!(Test-Path ".\SimSwap")) { Write-Host "Directory not found"; exit 1 }
+cd ".\SimSwap"
+
+Write-Host $PWD
 
 ForEach ($File in (Get-ChildItem -Path '.\*.py' -Recurse -File)) {
     $content = (Get-Content $File)
