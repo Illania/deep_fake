f287a84ca8522a040c13e8370634352e2dd143f6
Anna Gulich <ag@nordic-it.com>
2023-04-18 18:54:06 +0300

    added separate methods for single and multi swap, refactoring, updated demo folder structure

--- SwapManager.py
+++ SwapManager.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import cv2
+import numpy as np
 from PIL import Image
 import torch
 import torch.nn.functional as F
@@ -9,19 +10,19 @@ from models.models import create_model
 from options.test_options import TestOptions
 from util.videoswap_multispecific import video_swap
 from utils.hasher import Hasher, HashAlgorithm
+from util.videoswap import video_swap as video_swap_single
 
 
 class SwapManager:
 
-    def __init__(self, app, video_path, output_path, multispecific_dir):
+    def __init__(self, app):
         self.__app = app
-        self.__options = self.__init_options(video_path, output_path, multispecific_dir)
+        self.__options = self.__init_options()
         self.__model = self.__init_model(self.__options)
-        self.__crop_size = self.__options.crop_size
-        self.__multisepcific_dir = self.__options.multisepcific_dir
         self.__transformer_arcface = self.__init_transformer()
 
-    def swap(self):
+    def swap_multi(self, video_path, output_path, multispecific_dir):
+        self.__update_pathes(video_path, output_path, None, multispecific_dir)
         options = self.__options
         source_specific_id_nonorm_list = self.__prepare_source_persons()
         target_id_norm_list = self.__prepare_target_persons()
@@ -33,7 +34,32 @@ class SwapManager:
                    no_simswaplogo=options.no_simswaplogo,
                    use_mask=options.use_mask)
 
-        Hasher.check_hash_equals(HashAlgorithm.SHA1, self.__options.output_path, '../demo/output.mp4')
+        Hasher.check_hash_equals(HashAlgorithm.SHA1, self.__options.output_path, '../demo/multi/output.mp4')
+
+    def swap_single(self, video_path, output_path, pic_a_path):
+        self.__update_pathes(video_path, output_path, pic_a_path, None)
+        options = self.__options
+        with torch.no_grad():
+            pic_a = options.pic_a_path
+            img_a_whole = cv2.imread(pic_a)
+            img_a_align_crop, _ = self.__app.get(img_a_whole, self.__options.crop_size)
+            img_a_align_crop_pil = Image.fromarray(cv2.cvtColor(img_a_align_crop[0], cv2.COLOR_BGR2RGB))
+            img_a = self.__transformer_arcface(img_a_align_crop_pil)
+            img_id = img_a.view(-1, img_a.shape[0], img_a.shape[1], img_a.shape[2])
+
+            # convert numpy to tensor
+            img_id = img_id.cpu()
+
+            # create latent id
+            img_id_downsample = F.interpolate(img_id, size=(112, 112))
+            latend_id = self.__model.netArc(img_id_downsample)
+            latend_id = latend_id.detach().to('cpu')
+            latend_id = latend_id / np.linalg.norm(latend_id, axis=1, keepdims=True)
+            latend_id = latend_id.to('cpu')
+
+            video_swap_single(options.video_path, latend_id, self.__model, self.__app, options.output_path,
+                              temp_results_dir=options.temp_path, use_mask=options.use_mask)
+            Hasher.check_hash_equals(HashAlgorithm.SHA1, self.__options.output_path, '../demo/single/output.mp4')
 
     def __init_transformer(self):
         return transforms.Compose([
@@ -41,15 +67,12 @@ class SwapManager:
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
-    def __init_options(self, video_path, output_path, multispecific_dir):
+    def __init_options(self):
         opt = TestOptions()
         opt.initialize()
         opt.gpu_ids = -1
         opt.parser.add_argument('-f')
         opt = opt.parse()
-        opt.multisepcific_dir = multispecific_dir
-        opt.video_path = video_path
-        opt.output_path = output_path
         opt.temp_path = './tmp'
         opt.Arc_path = './arcface_model/arcface_checkpoint.tar'
         opt.isTrain = False
@@ -57,6 +80,12 @@ class SwapManager:
         opt.name = 'people'
         opt.use_mask = True
         return opt
+
+    def __update_pathes(self, video_path, output_path, pic_a_path, multispecific_dir):
+        self.__options.video_path = video_path
+        self.__options.output_path = output_path
+        self.__options.pic_a_path = pic_a_path
+        self.__options.multisepcific_dir = multispecific_dir
 
     def __init_model(self, options):
         torch.nn.Module.dump_patches = True
@@ -66,13 +95,13 @@ class SwapManager:
 
     def __prepare_source_persons(self):
         source_specific_id_nonorm_list = []
-        source_path = os.path.join(self.__multisepcific_dir, 'SRC_*')
+        source_path = os.path.join(self.__options.multisepcific_dir, 'SRC_*')
         source_specific_images_path = sorted(glob.glob(source_path))
 
         with torch.no_grad():
             for source_specific_image_path in source_specific_images_path:
                 specific_person_whole = cv2.imread(source_specific_image_path)
-                specific_person_align_crop, _ = self.__app.get(specific_person_whole, self.__crop_size)
+                specific_person_align_crop, _ = self.__app.get(specific_person_whole, self.__options.crop_size)
                 specific_person_align_crop_pil = Image.fromarray(
                     cv2.cvtColor(specific_person_align_crop[0], cv2.COLOR_BGR2RGB))
                 specific_person = self.__transformer_arcface(specific_person_align_crop_pil)
@@ -89,12 +118,12 @@ class SwapManager:
 
     def __prepare_target_persons(self):
         target_id_norm_list = []
-        target_path = os.path.join(self.__multisepcific_dir, 'DST_*')
+        target_path = os.path.join(self.__options.multisepcific_dir, 'DST_*')
         target_images_path = sorted(glob.glob(target_path))
 
         for target_image_path in target_images_path:
             img_a_whole = cv2.imread(target_image_path)
-            img_a_align_crop, _ = self.__app.get(img_a_whole, self.__crop_size)
+            img_a_align_crop, _ = self.__app.get(img_a_whole, self.__options.crop_size)
             img_a_align_crop_pil = Image.fromarray(cv2.cvtColor(img_a_align_crop[0], cv2.COLOR_BGR2RGB))
             img_a = self.__transformer_arcface(img_a_align_crop_pil)
             img_id = img_a.view(-1, img_a.shape[0], img_a.shape[1], img_a.shape[2])
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import os
 from FaceDetectAntelopeModel import FaceDetectAntelopeModel
 from SwapManager import SwapManager
 
-
 if __name__ == "__main__":
     os.chdir("SimSwap")
+
     app = FaceDetectAntelopeModel(0.6, 640)
-    swapManager = SwapManager(app, '../demo/input.mp4',
-                                   '../demo/output/output.mp4',
-                                   '../demo/multispecific')
-    swapManager.swap()
+    swapManager = SwapManager(app)
+
+    swapManager.swap_single('../demo/single/input.mp4',
+                            '../demo/single/results/result.mp4',
+                            '../demo/single/dst.jpeg')
+
+    swapManager.swap_multi('../demo/multi/input.mp4',
+                           '../demo/multi/results/output.mp4',
+                           '../demo/multi/multispecific')
